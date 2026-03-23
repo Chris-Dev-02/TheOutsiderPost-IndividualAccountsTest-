9c8f98ccdf8b20df47fff1e1d893a5c7aaf847d7
Chris <cmartinez.bitworks.com.sv>
2026-03-22 20:00:49 -0600

    Add entities to ApplicationDbContext

--- TheOutsiderPost.Infrastructure/Data/ApplicationDbContext.cs
+++ TheOutsiderPost.Infrastructure/Data/ApplicationDbContext.cs
@@ -10,5 +10,27 @@ namespace IndividualAccountsTest.Data
             : base(options)
         {
         }
+
+        public DbSet<Post> Posts => Set<Post>();
+        public DbSet<PostVersion> PostVersions => Set<PostVersion>();
+        public DbSet<ContentBlock> ContentBlocks => Set<ContentBlock>();
+
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<PostCategory> PostCategories => Set<PostCategory>();
+
+        public DbSet<Comment> Comments => Set<Comment>();
+        public DbSet<CommentReport> CommentReports => Set<CommentReport>();
+
+        public DbSet<PostReaction> PostReactions => Set<PostReaction>();
+        public DbSet<CommentReaction> CommentReactions => Set<CommentReaction>();
+
+        public DbSet<UserFavoritePost> UserFavoritePosts => Set<UserFavoritePost>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
     }
 }
