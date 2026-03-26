5af1bde7de655ed20c8346f53f95f3b9611380aa
Chris <cmartinez.bitworks.com.sv>
2026-03-25 22:00:04 -0600

    Add Post repository

---
+++ TheOutsiderPost.Application/Contracts/IPostRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheOutsiderPost.Domain.Entities;
+
+namespace TheOutsiderPost.Application.Contracts
+{
+    public interface IPostRepository
+    {
+        Task SaveChangesAsync();
+
+        Task<bool> SlugExistsAsync(string slug);
+
+        Task AddAsync(Post post);
+
+        Task<Post?> GetByIdAsync(int id);
+    }
+}
