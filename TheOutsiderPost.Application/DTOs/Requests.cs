65ece664f9b307512c70c0d77d00afd79291ca38
Chris <cmartinez.bitworks.com.sv>
2026-03-25 22:01:15 -0600

    Add requests and responses records

---
+++ TheOutsiderPost.Application/DTOs/Requests.cs
@@ -0,0 +1,30 @@
+﻿using TheOutsiderPost.Domain.Enums;
+
+namespace TheOutsiderPost.Application.DTOs
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Slug"></param>
+    /// <param name="Title"></param>
+    /// <param name="Summary"></param>
+    /// <param name="CreatedBy"></param>
+    /// <param name="ContentBlocks"></param>
+    public record CreatePostRequest(
+        string Slug,
+        string Title,
+        string Summary,
+        string CreatedBy,
+        List<CreateContentBlockDto> ContentBlocks);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Type"></param>
+    /// <param name="Value"></param>
+    /// <param name="Order"></param>
+    public record CreateContentBlockDto(
+        ContentBlockType Type,
+        string Value,
+        int Order);
+}
---
+++ TheOutsiderPost.Application/DTOs/Responses.cs
@@ -0,0 +1,10 @@
+﻿namespace TheOutsiderPost.Application.DTOs
+{
+    /// <summary>
+    /// Represents the response returned after successfully creating a post.
+    /// </summary>
+    /// <remarks>This record encapsulates the identifier of the newly created post. Use the <see
+    /// cref="PostId"/> property to access the unique identifier assigned to the post.</remarks>
+    /// <param name="PostId"></param>
+    public record CreatePostResponse(int PostId);
+}
